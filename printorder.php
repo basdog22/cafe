926ec874f010cf399b57bc88e531c52e2c09b5cd
jslemmer <jslemmer@users.noreply.github.com>
2016-03-29 10:51:16 +0800

    Adding of actual print command

--- printorder.php
+++ printorder.php
@@ -1,5 +1,6 @@
 <?php
-	
-shell_exec('php receipt.php > receipts/receipt.txt');	
-	
+
+shell_exec('php receipt.php > receipts/receipt.txt');
+shell_exec('lpr -o raw -H localhost -P POS58 receipts/receipt.txt');
+
 ?>
