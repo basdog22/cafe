7d4012ba84f5e160f1afd012c1bf3ee8b97070ff
jslemmer <jslemmer@users.noreply.github.com>
2016-03-29 12:13:26 +0800

    Receipt update (dynamic)
    Including db connection
    Initial, still very rough

--- receipt.php
+++ receipt.php
@@ -1,4 +1,6 @@
 <?php
+
+require __DIR__ . '/require/db.php';
 require __DIR__ . '/printer/autoload.php';
 use Mike42\Escpos\Printer;
 use Mike42\Escpos\EscposImage;
@@ -7,35 +9,76 @@ use Mike42\Escpos\PrintConnectors\FilePrintConnector;
 /* Fill in your own connector here */
 $connector = new FilePrintConnector("php://stdout");
 
-/* Information for the receipt */
-$items = array(
-        new item("Ultimate", "4.00"),
-        new item("Tuna Salad", "3.50"),
-        new item("Americano", "1.00"),
-        new item("Extra bacon", "4.45"),
-);
-
-//$subtotal = new item('Subtotal', '12.95');
-//$tax = new item('A local tax', '1.30');
-$total = new item('Total', '14.25', true);
-/* Date is kept the same for testing */
-// $date = date('l jS \of F Y h:i:s A');
-$date = "Monday 6th of April 2015 02:56:25 PM";
-
 /* Start the printer */
-//$logo = new EscposImage("resources/escpos-php.png");
 $printer = new Printer($connector);
 
-/* Print top logo */
-$printer -> setJustification(Printer::JUSTIFY_CENTER);
-//$printer -> graphics($logo);
+if(isset($_GET['id'])){
+	$id = $_GET['id'];
+}
 
-/* Name of shop */
+// Select customer info
+$sql_orders = "select Order_id,o.cus_id,firstname as fname,lastname as lname,Date,Time,payed from orders as o LEFT JOIN customer_info as c ON o.cus_id = c.cus_id WHERE order_id = $id";
+$result = $mysql->query($sql_orders);
+$row_order = $mysql->fetch($result);
+
+if(empty($row_order['lname']) && empty($row_order['fname'])) {
+	$cusname='Unknown';
+} else {
+	$cusname=$row_order['fname']."&nbsp".$row_order['lname'];
+}
+
+/* Name of customer & order# */
+$printer -> setJustification(Printer::JUSTIFY_CENTER);
 $printer -> selectPrintMode(Printer::MODE_DOUBLE_WIDTH);
-$printer -> text("Richard\n");
+$printer -> text("$cusname\n");
 $printer -> selectPrintMode();
-$printer -> text("Order# 145\n");
+$printer -> text("Order# ". $row_order['Order_id'] . "\n");
 $printer -> feed();
+
+//echo $cusname . "<br/>";
+//echo "Order#" . $row_order['Order_id'] . "<br/><br/>";
+
+$total_cost = 0;
+
+/* Information for the receipt */
+$items = array();
+
+$sql_item_detail = "SELECT Item_id,F.order_id,cus.lastname as lname,Cs.cata_name as Food_name,Cp.Cata_name,Quantity,Cs.price as Single_Price,(Cs.price*quantity)as Total_Price,F.food_id from order_food as F JOIN orders as O on F.order_id = O.order_id JOIN food_catalogue as Cs ON F.food_id = Cs.food_id JOIN food_catalogue as Cp ON Cp.food_id = Cs.catalog_id LEFT JOIN customer_info as cus ON cus.cus_id = O.cus_id WHERE F.order_id= $id";
+$result_item_detail = $mysql->query($sql_item_detail);
+while($row_item_detail = $mysql->fetch($result_item_detail)) {
+	// print row and quantity
+	//echo $row_item_detail['Food_name'] . " (" . $row_item_detail['Quantity'] . ")";
+	//echo "&#165;" . $row_item_detail['Total_Price'] . "<br/>";
+	
+	/* Information for the receipt */	
+	array_push($items, new item($row_item_detail['Food_name'], $row_item_detail['Total_Price']));
+	
+	$total_cost = $total_cost + $row_item_detail['Total_Price'];
+}
+
+//echo "&#165; " . $total;
+
+// Print all lines
+$printer -> setEmphasis(false);
+foreach($items as $item) {
+        $printer -> text($item);
+}
+
+// Total cost
+$total = new item('Total', $total_cost, true);
+
+/* Total */
+$printer -> selectPrintMode(Printer::MODE_DOUBLE_WIDTH);
+$printer -> text($total);
+$printer -> selectPrintMode();
+
+
+
+
+
+/* Date is kept the same for testing */
+// $date = date('l jS \of F Y h:i:s A');
+//$date = "Monday 6th of April 2015 02:56:25 PM";
 
 /* Title of receipt */
 //$printer -> setEmphasis(true);
@@ -44,23 +87,16 @@ $printer -> feed();
 
 /* Items */
 //$printer -> setJustification(Printer::JUSTIFY_LEFT);
-$printer -> setEmphasis(true);
+//$printer -> setEmphasis(true);
 //$printer -> text(new item('', '$'));
-$printer -> setEmphasis(false);
-foreach($items as $item) {
-        $printer -> text($item);
-}
 
-$printer -> setEmphasis(true);
+
+//$printer -> setEmphasis(true);
 //$printer -> text($subtotal);
-$printer -> setEmphasis(false);
-$printer -> feed();
+//$printer -> setEmphasis(false);
+//$printer -> feed();
 
-/* Tax and total */
-//$printer -> text($tax);
-$printer -> selectPrintMode(Printer::MODE_DOUBLE_WIDTH);
-$printer -> text($total);
-$printer -> selectPrintMode();
+
 
 /* Footer */
 $printer -> feed(2);
